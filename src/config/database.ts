f1657c08b19744543ad90b247c87a26e5d746268
zevdvlpr <zevdvlpr@gmail.com>
2021-01-22 22:22:05 -0300

    :beetle: fix: env

--- src/config/database.ts
+++
@@ -1,9 +0,0 @@
-const databaseConfig = {
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-};
-
-export { databaseConfig };
