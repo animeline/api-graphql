57ad276abc2624fe7db17d7f3aadc0a7769e9319
zevdvlpr <zevdvlpr@gmail.com>
2021-01-27 16:05:41 -0300

    :card_file_box:  db: entities

---
+++ src/modules/users/infra/database/typeorm/entities/User.ts
@@ -0,0 +1,30 @@
+import { Exclude } from 'class-transformer';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  email!: string;
+
+  @Column()
+  @Exclude()
+  password!: string;
+
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @UpdateDateColumn()
+  updated_at!: Date;
+}
